19c98d5a440a2301dd3638a8a64a9c048a88a65a
Ramesh T <ramesht@op5.com>
2016-09-27 07:18:27 +0200

    MON-9545 documentation link updated

--- modules/monitoring/helpers/performance_data.php
+++ modules/monitoring/helpers/performance_data.php
@@ -72,7 +72,7 @@ class performance_data {
 	 * @param $threshold_string
 	 * @param $value
 	 * @return bool
-	 * @link https://www.monitoring-plugins.org/doc/guidelines.html
+	 * @link <a href="https://www.monitoring-plugins.org/doc/guidelines.html#THRESHOLDFORMAT">Threshold and ranges</a>
 	 */
 	public function match_threshold($threshold_string, $value) {
 		//Check threshold string empty
